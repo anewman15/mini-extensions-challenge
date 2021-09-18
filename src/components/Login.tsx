36fbe84152312aa1a1bab3a2d317b9ee8e52d1c9
Abdullah Numan <anewman15@hotmail.com>
2021-09-18 12:54:29 +0600

    Create Login comp

---
+++ src/components/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <label>
+          Enter name
+        <input type="text" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
