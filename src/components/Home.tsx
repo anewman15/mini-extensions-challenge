eb08a232d7a5da33a26d3817411733844dcdbfad
Abdullah Numan <anewman15@hotmail.com>
2021-09-28 21:39:43 +0600

    Modify Home comp

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,12 @@
-import React from 'react'
 import Login from './Login';
 import UserPage from './UserPage';
 
 const Home = () => {
-  const classes = [
-    {
-      name: 'CS01',
-      students: ['AKalam', 'Hasina', 'Nadira']
-    }
-  ];
-  
+
   return (
     <div>
       <Login />
-      <UserPage classes={classes}/>
+      <UserPage/>
     </div>
   )
 }
