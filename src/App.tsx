dc97771fcd15ad6d268004e4f955b310bee4584a
Abdullah Numan <anewman15@hotmail.com>
2021-09-28 21:39:01 +0600

    Create store and add to App with Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 import Home from './components/Home';
+import store from './redux/store/store';
 
 function App() {
 
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Home />
+      </div>
+    </Provider>
   );
 }
 
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,30 +1,41 @@
 import { createStore } from 'redux';
 import combinedReducer from '../reducers';
 
+// type initialStateType = {
+//   user: Record<FieldSet>,
+//   classes: Records<FieldSet>,
+//   students: {
+//     [key: string]: string
+//   }
+// };
+
+const initialState: any = {
+  user: {
+    id: '',
+    fields: {
+      Name: '',
+      Classes: [
+        '', ''
+      ],
+    }
+  },
+  classes: [
+    {
+      id: '',
+      fields: {
+        Name: '',
+        Students: [],
+      }
+    }
+  ],
+  students: {
+    "": "",
+  }
+};
+
 const store = createStore(
   combinedReducer,
-  {
-    user: {
-      id: 'dfsf',
-      fields: {
-        Name: 'Silly',
-        Classes: [
-        ],
-      }
-    },
-    classes: [
-      {
-        id: 'sdasd',
-        fields: {
-          Name: 'sdfsdfsdg',
-          Students: [],
-        }
-      }
-    ],
-    students: {
-      "dsdf": "sdsf",
-    }
-  }
+  initialState,
 );
 
 export default store;
