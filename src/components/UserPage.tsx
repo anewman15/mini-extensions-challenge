2c1b305cbb71f3b74896305a5fde95690d79c0ce
Abdullah Numan <anewman15@hotmail.com>
2021-09-28 21:41:13 +0600

    Modify UserPage comp

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
 
 type UserPageProps = {
-  classes: {
-    name: string,
-    students: string[]
-  }[]
-}
-const UserPage = ( { classes }: UserPageProps) => {
+
+};
+
+const UserPage = () => {
   return (
     <div>
-      {
-        classes.map(cl => {
-          return (
-            <div key={cl.name}>
-              {cl.name}
-              <div>
-                {
-                  cl.students.map(student => {
-                    return (
-                      <span>{student}&nbsp;</span>
-                    );
-                  })
-                }
-              </div>
-            </div>
-          );
-        })
-      }
+      Hi
     </div>
   );
 }
